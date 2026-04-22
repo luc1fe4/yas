7693641d04ccc0a2b4c93d4ba7a924a25edf8c9b
Le Cong Phuc <lcphuc.1105@gmail.com>
2026-04-23 03:22:37 +0700

    fix variable

--- order/src/main/java/com/yas/order/mapper/OrderMapper.java
+++ order/src/main/java/com/yas/order/mapper/OrderMapper.java
@@ -5,12 +5,15 @@ import com.yas.order.viewmodel.order.OrderBriefVm;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
+import org.mapstruct.ReportingPolicy;
+import com.yas.order.model.Order;
+import com.yas.order.viewmodel.order.OrderVm;
+import com.yas.order.viewmodel.order.OrderBriefVm;
 
 @Mapper(
     componentModel = "spring",
     unmappedTargetPolicy = ReportingPolicy.WARN  
 )
 public interface OrderMapper {
-    @Mapping(target = "someField", ignore = true)  
-    OrderVm toVm(Order order);
+    OrderItemCsv toCsv(OrderBriefVm orderBriefVm);
 }
