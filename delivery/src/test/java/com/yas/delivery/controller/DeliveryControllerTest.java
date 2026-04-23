b620992517f12a2e7c9a2f02a9645450589cf8e8
23120292 <nakrothnguyen127@gmail.com>
2026-04-23 15:17:39 +0700

    fix compilation error in DeliveryControllerTest by updating imports and dependencies

--- delivery/src/test/java/com/yas/delivery/controller/DeliveryControllerTest.java
+++ delivery/src/test/java/com/yas/delivery/controller/DeliveryControllerTest.java
@@ -7,8 +7,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import com.yas.delivery.service.DeliveryService;
 
@@ -18,7 +18,7 @@ class DeliveryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private DeliveryService deliveryService;
 
     @Test
