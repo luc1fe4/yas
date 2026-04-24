f9e9838821804ae621070b35d0c692de6be1ce65
23120292 <nakrothnguyen127@gmail.com>
2026-04-24 18:51:49 +0700

    IllegalState ApplicationContext

--- tax/src/test/java/com/yas/tax/controller/TestApplication.java
+++ tax/src/test/java/com/yas/tax/controller/TestApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "com.yas.tax.controller", exclude = {
-    org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration.class,
-    org.springframework.boot.security.oauth2.server.resource.autoconfigure.servlet.OAuth2ResourceServerAutoConfiguration.class,
-    org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration.class,
-    org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration.class
+    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
 })
-@ImportAutoConfiguration(org.springframework.boot.jackson.autoconfigure.JacksonAutoConfiguration.class)
+@ImportAutoConfiguration(org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class)
 public class TestApplication {
 }
