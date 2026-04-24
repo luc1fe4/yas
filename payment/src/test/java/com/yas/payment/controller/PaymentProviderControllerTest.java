8836435cd0a340f1175791f7752eb9a4a7c836cf
23120292 <nakrothnguyen127@gmail.com>
2026-04-24 20:20:46 +0700

    fix: PaymentProviderControllerTest

--- payment/src/test/java/com/yas/payment/controller/PaymentProviderControllerTest.java
+++ payment/src/test/java/com/yas/payment/controller/PaymentProviderControllerTest.java
@@ -7,11 +7,12 @@ import com.yas.payment.viewmodel.paymentprovider.PaymentProviderVm;
 import com.yas.payment.viewmodel.paymentprovider.UpdatePaymentVm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
