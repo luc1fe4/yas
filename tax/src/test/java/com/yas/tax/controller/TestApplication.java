ef58b34f25858473d857ba2513fc848e7c686606
23120292 <nakrothnguyen127@gmail.com>
2026-04-24 18:40:19 +0700

    fix: update SB4 auto-config paths in TestApplication

--- tax/src/test/java/com/yas/tax/controller/TestApplication.java
+++ tax/src/test/java/com/yas/tax/controller/TestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 @SpringBootApplication(scanBasePackages = "com.yas.tax.controller", exclude = {
     org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration.class,
     org.springframework.boot.security.oauth2.server.resource.autoconfigure.servlet.OAuth2ResourceServerAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+    org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration.class,
+    org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration.class
 })
 @ImportAutoConfiguration(org.springframework.boot.jackson.autoconfigure.JacksonAutoConfiguration.class)
 public class TestApplication {
