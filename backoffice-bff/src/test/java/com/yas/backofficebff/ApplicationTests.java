dd11606f91d34c1440ede619f650232b73c6bada
23120292 <nakrothnguyen127@gmail.com>
2026-05-03 18:06:13 +0700

    fix(ci): use non-empty dummy OAuth2 URIs and fix Jenkinsfile syntax error

--- backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
@@ -7,7 +7,10 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(properties = {
     "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.oauth2.client.reactive.ReactiveOAuth2ClientAutoConfiguration,org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration",
-    "spring.security.oauth2.client.provider.keycloak.issuer-uri=",
+    "spring.security.oauth2.client.provider.keycloak.issuer-uri=http://localhost:9999",
+    "spring.security.oauth2.client.provider.keycloak.authorization-uri=http://localhost:9999/auth",
+    "spring.security.oauth2.client.provider.keycloak.token-uri=http://localhost:9999/token",
+    "spring.security.oauth2.client.provider.keycloak.jwk-set-uri=http://localhost:9999/jwks",
     "spring.data.redis.host=localhost",
     "spring.data.redis.port=6379"
 })
