a309208ef0c405eb1a7cddc2df73dad70723b883
23120292 <nakrothnguyen127@gmail.com>
2026-05-03 18:14:09 +0700

    fix(ci): mock SecurityWebFilterChain and nullify issuer-uri in test profile

--- backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
@@ -4,14 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true")
 @ActiveProfiles("test")
 class ApplicationTests {
 
     @MockitoBean
     private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    @MockitoBean
+    private SecurityWebFilterChain securityWebFilterChain;
 
     @Test
     void contextLoads() {
