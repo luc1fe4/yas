9168868a014993504b9baca65a1f599d11097c57
23120292 <nakrothnguyen127@gmail.com>
2026-05-03 17:48:57 +0700

    fix(ci): bypass backoffice-bff oauth2 discovery by excluding autoconfiguration

--- backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
@@ -6,10 +6,7 @@ import org.springframework.security.oauth2.client.registration.ReactiveClientReg
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(properties = {
-    "spring.security.oauth2.client.provider.keycloak.issuer-uri=http://localhost:9999/realms/Yas",
-    "spring.security.oauth2.client.provider.keycloak.authorization-uri=http://localhost:9999/auth",
-    "spring.security.oauth2.client.provider.keycloak.token-uri=http://localhost:9999/token",
-    "spring.security.oauth2.client.provider.keycloak.jwk-set-uri=http://localhost:9999/jwks",
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.oauth2.client.reactive.ReactiveOAuth2ClientAutoConfiguration",
     "spring.data.redis.host=localhost",
     "spring.data.redis.port=6379"
 })
