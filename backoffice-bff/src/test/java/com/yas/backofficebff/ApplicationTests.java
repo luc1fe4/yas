f2d7351096c38514ecbd6b19a3fb042c6a930264
23120292 <nakrothnguyen127@gmail.com>
2026-05-03 17:48:19 +0700

    fix(ci): fix backoffice-bff context loading failure by mocking Redis and OAuth2

--- backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/ApplicationTests.java
@@ -6,10 +6,12 @@ import org.springframework.security.oauth2.client.registration.ReactiveClientReg
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(properties = {
-    "spring.security.oauth2.client.provider.keycloak.issuer-uri=",
+    "spring.security.oauth2.client.provider.keycloak.issuer-uri=http://localhost:9999/realms/Yas",
     "spring.security.oauth2.client.provider.keycloak.authorization-uri=http://localhost:9999/auth",
     "spring.security.oauth2.client.provider.keycloak.token-uri=http://localhost:9999/token",
-    "spring.security.oauth2.client.provider.keycloak.jwk-set-uri=http://localhost:9999/jwks"
+    "spring.security.oauth2.client.provider.keycloak.jwk-set-uri=http://localhost:9999/jwks",
+    "spring.data.redis.host=localhost",
+    "spring.data.redis.port=6379"
 })
 class ApplicationTests {
 
