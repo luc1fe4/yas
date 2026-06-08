d523c0380ef54ccb7710652b3c4755fdcea6302d
23120292 <nakrothnguyen127@gmail.com>
2026-06-08 13:50:51 +0700

    feat: support deploy to custom namespace in deploy-yas-applications.sh

--- k8s/deploy/deploy-yas-applications.sh
+++ k8s/deploy/deploy-yas-applications.sh
@@ -5,41 +5,45 @@ set -x
 helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo update
 
+# Namespace parameter (defaults to dev)
+NAMESPACE=${1:-dev}
+
 read -rd '' DOMAIN \
 < <(yq -r '.domain' ./cluster-config.yaml)
 
 helm dependency build ../charts/backoffice-bff
 helm upgrade --install backoffice-bff ../charts/backoffice-bff \
---namespace yas --create-namespace \
---set backend.ingress.host="backoffice.$DOMAIN"
+--namespace "$NAMESPACE" --create-namespace \
+--set backend.ingress.host="backoffice.$NAMESPACE.$DOMAIN"
 
 helm dependency build ../charts/backoffice-ui
 helm upgrade --install backoffice-ui ../charts/backoffice-ui \
---namespace yas --create-namespace
+--namespace "$NAMESPACE" --create-namespace
 
 sleep 60
 
 helm dependency build ../charts/storefront-bff
 helm upgrade --install storefront-bff ../charts/storefront-bff \
---namespace yas --create-namespace \
---set backend.ingress.host="storefront.$DOMAIN"
+--namespace "$NAMESPACE" --create-namespace \
+--set backend.ingress.host="storefront.$NAMESPACE.$DOMAIN"
 
 helm dependency build ../charts/storefront-ui
 helm upgrade --install storefront-ui ../charts/storefront-ui \
---namespace yas --create-namespace
+--namespace "$NAMESPACE" --create-namespace
 
 sleep 60
 
 helm upgrade --install swagger-ui ../charts/swagger-ui \
---namespace yas --create-namespace \
---set ingress.host="api.$DOMAIN"
+--namespace "$NAMESPACE" --create-namespace \
+--set ingress.host="api.$NAMESPACE.$DOMAIN"
 
 sleep 20
 
 for chart in {"cart","customer","inventory","location","media","order","payment","payment-paypal","product","promotion","rating","search","tax","recommendation","webhook","sampledata"} ; do
     helm dependency build ../charts/"$chart"
     helm upgrade --install "$chart" ../charts/"$chart" \
-    --namespace yas --create-namespace \
-    --set backend.ingress.host="api.$DOMAIN"
+    --namespace "$NAMESPACE" --create-namespace \
+    --set backend.ingress.host="api.$NAMESPACE.$DOMAIN"
     sleep 60
 done
+
